285f00b9867f09db1fd30aa5654b38119cc8dde5
Marcos Kees <merkosas@protonmail.com>
2024-08-18 01:31:58 +0000

    feat: Add symbol and name lookup

--- element.sh
+++ element.sh
@@ -7,6 +7,11 @@ get_element_by_atomic_number() {
   echo "$ELEMENT"
 }
 
+get_element_by_name_or_symbol() {
+  ELEMENT=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE name='$1' OR symbol='$1'")
+  echo "$ELEMENT"
+}
+
 if [[ -z $1 ]]
 then
   echo "Please provide an element as an argument."
@@ -14,5 +19,7 @@ else
  if [[ $1 =~ ^[0-9]+$ ]]
 then
   ELEMENT=$(get_element_by_atomic_number $1)
-  # ... resto del código
+else
+  ELEMENT=$(get_element_by_name_or_symbol $1)
 fi
+
